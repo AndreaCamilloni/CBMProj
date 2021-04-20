77ae6183446c4623f2f18e199ffa3663a1de370f
Andrea Camilloni <s1090325@studenti.univpm.it>
2021-04-20 09:14:29 +0200

    diagnosis task

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from Dataset.Sequence import GenericImageSequence
 from Model import single_task_model
 
 model = single_task_model()
+df['diagnosis_numeric']=df['diagnosis_numeric'].apply(lambda x: 1 if x == 2 else 0) #MEL or NOT-MEL 'mapping'
+
 train_df, test_df = train_test_split(df, test_size=0.3) #splittare secondo train e test indexes
-train_gen = GenericImageSequence(train_df,'derm','diagnosis_numeric', batch_size=1)
-test_gen = GenericImageSequence(test_df,'derm','diagnosis_numeric', batch_size=1)
+train_gen = GenericImageSequence(train_df,'derm','diagnosis_numeric', batch_size=16)
+test_gen = GenericImageSequence(test_df,'derm','diagnosis_numeric', batch_size=16)
 
 
 early_stopping = callbacks.EarlyStopping(
@@ -22,8 +24,8 @@ early_stopping = callbacks.EarlyStopping(
 history = model.fit(
     train_gen,
     validation_data=test_gen,
-    steps_per_epoch=200,
-    validation_steps=100,
+    steps_per_epoch=30,
+    validation_steps=10,
     epochs=5)
 
 #train_gen[0]
