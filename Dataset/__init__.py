ba1a5a5cede7fc738c89c472ce796dc4c46f2fae
Andrea Camilloni <s1090325@studenti.univpm.it>
2021-04-27 11:08:05 +0200

    MEL - NEV "mapping"

--- Dataset/__init__.py
+++ Dataset/__init__.py
@@ -5,7 +5,12 @@ meta_dir = os.path.join('..', '/home/andreac/release_v0/meta')
 image_dir = os.path.join('..', '/home/andreac/release_v0/images')
 
 df = pd.read_csv(os.path.join(meta_dir, 'dataframe.csv'))
-df['diagnosis_numeric']=df['diagnosis_numeric'].apply(lambda x: 1 if x == 2 else 0) #MEL or NOT-MEL 'mapping'
+#MEL && NEV cases mapping
+df['diagnosis_numeric']=df['diagnosis_numeric'].apply(lambda x: 10 if ((x == 4) or (x == 3) or (x == 0)) else x)
+delete_row = df[df['diagnosis_numeric']==10].index
+df = df.drop(delete_row)
+df['diagnosis_numeric']=df['diagnosis_numeric'].apply(lambda x: 1 if x == 2 else 0)
+
 
 train_indexes = list(pd.read_csv(os.path.join(meta_dir, 'train_indexes.csv'))['indexes'])
 valid_indexes = list(pd.read_csv(os.path.join(meta_dir, 'valid_indexes.csv'))['indexes'])
