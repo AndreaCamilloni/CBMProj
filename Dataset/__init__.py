fdf873e286edc6ab9d8e5ae422c038963dfed226
Andrea Camilloni <s1090325@studenti.univpm.it>
2021-04-27 14:06:08 +0200

    adjustment nev-mel mapping

--- Dataset/__init__.py
+++ Dataset/__init__.py
@@ -15,13 +15,12 @@ valid_df = df.iloc[valid_indexes]
 test_df = df.iloc[test_indexes]
 
 #MEL && NEV cases mapping
-def binary_diagnosis(df):
-    df['diagnosis_numeric']=df['diagnosis_numeric'].apply(lambda x: 10 if ((x == 4) or (x == 3) or (x == 0)) else x)
-    delete_row = df[df['diagnosis_numeric']==10].index
-    df = df.drop(delete_row)
+def nev_or_mel(df):
+    df = df.drop(df[df.diagnosis_numeric==0].index)
+    df = df.drop(df[df.diagnosis_numeric==3].index)
+    df = df.drop(df[df.diagnosis_numeric==4].index)
     df['diagnosis_numeric']=df['diagnosis_numeric'].apply(lambda x: 1 if x == 2 else 0)
     return df
-
-train_df = binary_diagnosis(train_df)
-valid_df = binary_diagnosis(valid_df)
-test_df = binary_diagnosis(test_df)
+train_df = nev_or_mel(train_df)
+valid_df = nev_or_mel(valid_df)
+test_df = nev_or_mel(test_df)
