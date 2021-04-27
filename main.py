bfe52e17e81231885012a1d8ceab83cbcb379f05
Andrea Camilloni <s1090325@studenti.univpm.it>
2021-04-27 14:38:15 +0200

    confusion matrix

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from sklearn.model_selection import train_test_split
 from tensorflow import keras
 from tensorflow.keras import *
 import pandas as pd
+import numpy as np
 from Dataset import df, train_df, test_df, valid_df
 from Dataset.Sequence import GenericImageSequence
 from Model import single_task_model
 import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix
 
 model = single_task_model()
 
@@ -39,7 +41,7 @@ plt.plot(history.history['val_categorical_accuracy'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
+plt.legend(['train', 'valid'], loc='upper left')
 plt.savefig('accuracy.png')
 plt.close()
 # summarize history for loss
@@ -48,6 +50,22 @@ plt.plot(history.history['val_loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
+plt.legend(['train', 'valid'], loc='upper left')
 plt.savefig('loss.png')
 plt.close()
+
+Y_pred = model.predict(test_gen)
+#
+y_pred = np.argmax(Y_pred, axis=1)
+cm=confusion_matrix(test_df.diagnosis_numeric, y_pred)
+plt.imshow(cm, cmap=plt.cm.Blues)
+plt.xlabel("Predicted labels")
+plt.ylabel("True labels")
+plt.xticks([], [])
+plt.yticks([], [])
+plt.title('Confusion matrix ')
+plt.colorbar()
+plt.savefig('cm.png')
+plt.close()
+
+print(classification_report(test_df.diagnosis_numeric, y_pred))
