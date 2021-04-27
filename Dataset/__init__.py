950338f486f85338bdcdd5b9d5c573760636b9c0
Andrea Camilloni <s1090325@studenti.univpm.it>
2021-04-27 11:44:10 +0200

    MEL && NEV cases mapping

--- Dataset/__init__.py
+++ Dataset/__init__.py
@@ -5,12 +5,6 @@ meta_dir = os.path.join('..', '/home/andreac/release_v0/meta')
 image_dir = os.path.join('..', '/home/andreac/release_v0/images')
 
 df = pd.read_csv(os.path.join(meta_dir, 'dataframe.csv'))
-#MEL && NEV cases mapping
-df['diagnosis_numeric']=df['diagnosis_numeric'].apply(lambda x: 10 if ((x == 4) or (x == 3) or (x == 0)) else x)
-delete_row = df[df['diagnosis_numeric']==10].index
-df = df.drop(delete_row)
-df['diagnosis_numeric']=df['diagnosis_numeric'].apply(lambda x: 1 if x == 2 else 0)
-
 
 train_indexes = list(pd.read_csv(os.path.join(meta_dir, 'train_indexes.csv'))['indexes'])
 valid_indexes = list(pd.read_csv(os.path.join(meta_dir, 'valid_indexes.csv'))['indexes'])
@@ -19,3 +13,15 @@ test_indexes = list(pd.read_csv(os.path.join(meta_dir, 'test_indexes.csv'))['ind
 train_df = df.iloc[train_indexes]
 valid_df = df.iloc[valid_indexes]
 test_df = df.iloc[test_indexes]
+
+#MEL && NEV cases mapping
+def binary_diagnosis(df):
+    df['diagnosis_numeric']=df['diagnosis_numeric'].apply(lambda x: 10 if ((x == 4) or (x == 3) or (x == 0)) else x)
+    delete_row = df[df['diagnosis_numeric']==10].index
+    df = df.drop(delete_row)
+    df['diagnosis_numeric']=df['diagnosis_numeric'].apply(lambda x: 1 if x == 2 else 0)
+    return df
+
+train_df = binary_diagnosis(train_df)
+valid_df = binary_diagnosis(valid_df)
+test_df = binary_diagnosis(test_df)
