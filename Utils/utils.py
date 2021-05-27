4fb7ea8ce4c14a53fabe2e53cc3d65fd36173e6a
Andrea Camilloni <s1090325@studenti.univpm.it>
2021-05-27 10:21:52 +0200

    Add accuracy on stratifiedKfold

--- Utils/utils.py
+++ Utils/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib as mpl
 from sklearn.metrics import confusion_matrix, classification_report
 
-#mpl.rcParams.update({'font.size': 22, 'text.color': "black"})
+# mpl.rcParams.update({'font.size': 22, 'text.color': "black"})
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,47 +10,11 @@ import itertools
 
 from sklearn.utils import resample
 
+# from sklearn.model_selection import StratifiedKFold
+# Using 5-Fold Cross Validation
+from sklearn.model_selection import StratifiedKFold, cross_val_score
 
-def plot_confusion_matrix(cm,
-                          target_names,
-                          title='Confusion matrix',
-                          cmap=None,
-                          normalize=True):
-    accuracy = np.trace(cm) / float(np.sum(cm))
-    misclass = 1 - accuracy
-
-    if cmap is None:
-        cmap = plt.get_cmap('Blues')
-
-    plt.figure(figsize=(8, 6))
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-
-    if target_names is not None:
-        tick_marks = np.arange(len(target_names))
-        plt.xticks(tick_marks, target_names, rotation=45)
-        plt.yticks(tick_marks, target_names)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        if normalize:
-            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-        else:
-            plt.text(j, i, "{:,}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
-    plt.savefig("cm.png")
-    plt.close()
+import sys
 
 
 def balance_df(df):
@@ -65,20 +29,17 @@ def balance_df(df):
 def oversampling_balance_df(df):
     nev = df[df.diagnosis_numeric == 0]
     mel = df[df.diagnosis_numeric == 1]
-    nev_downsampled = resample(nev,
-                               replace=False,
-                               n_samples=len(mel))
-    return pd.concat([mel, nev_downsampled, mel]).sample(frac=1)
+    return pd.concat([mel, nev, mel]).sample(frac=1)
 
 
-def export_graph(_history, file_name, train, val, title, y_label, x_label='epoch', legend=['train', 'valid']):
+def export_graph(_history, file_name, train, val, title, y_label, x_label='epoch', legend=['train', 'valid'], path=''):
     plt.plot(_history.history[train])
     plt.plot(_history.history[val])
     plt.title(title)
     plt.ylabel(y_label)
     plt.xlabel(x_label)
     plt.legend(legend, loc='upper left')
-    plt.savefig(file_name)
+    plt.savefig(path + file_name)
     plt.close()
 
 
@@ -128,3 +89,14 @@ def plot_confusion(y_true, y_pred, labels, fontsize=18,
     ax.grid(b=False, which='major')
 
     return cax
+
+
+def accuracy_on_stratiefiedKfold(model, x_test, true_label, model_name):
+    stratifiedKFold = StratifiedKFold(n_splits=5)
+    results = cross_val_score(model, x_test, true_label, cv=stratifiedKFold, scoring='accuracy')
+    stdoutOrigin = sys.stdout
+    sys.stdout = open("CBM/" + model_name + ".txt", "w")
+    print(model_name + ": Results on KFold:", results,
+          "Mean Accuracy: %.3f%%, Standard Deviation: (%.3f%%)" % (results.mean() * 100.0, results.std() * 100.0))
+    sys.stdout.close()
+    sys.stdout = stdoutOrigin
--- script.py
+++ script.py
@@ -1,21 +1,26 @@
+""" CBM with logistic regression as classifier + 7pt algorithm """
 import os
 import pandas as pd
 from sklearn.metrics import classification_report
-
+import tensorflow as tf
 from Dataset import image_dir, train_df, test_df, labels_cols, labels_name, labels
 from Model.CBM import deep_features, logReg, logReg_predict
 from keras.applications.inception_v3 import InceptionV3, preprocess_input
+import keras
 
 import matplotlib as mpl
 
 from Utils.sevenPtRule import diagnosis
-from Utils.utils import plot_confusion, report_to_csv
+from Utils.utils import plot_confusion, report_to_csv, accuracy_on_stratiefiedKfold
 
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
+# base Model with weights pre-trained on imagenet: ResNet101V2, MobileNet, InceptionV3
 input_shape = (256, 256, 3)
-base_model = InceptionV3(include_top=False, input_shape=input_shape)
+base_model = keras.applications.ResNet101V2(include_top=False, input_shape=input_shape)
+
+
 
 # Features extraction from image inputs
 train_derm_f = deep_features(img_paths=[os.path.join(image_dir, img_path) for img_path in train_df.derm],
@@ -26,6 +31,7 @@ test_derm_f = deep_features(img_paths=[os.path.join(image_dir, img_path) for img
                             model=base_model,
                             func_preprocess_input=preprocess_input,
                             target_size=(256, 256, 3), )
+
 # SINGLE TASK DIAGNOSIS X => Y
 x_to_y = logReg(C=0.01, class_weight='balanced', train_inputs=train_derm_f,
                 train_label=train_df.diagnosis_numeric)
@@ -36,6 +42,9 @@ plt.savefig('CBM/Diagnosis/' + 'DIAG' + '_CM' + '_x_to_y')
 plt.close()
 # classification report to csv
 report_to_csv(y_singletask, test_df.diagnosis_numeric, "CBM/Diagnosis/result_x_to_y.csv")
+# accuracy on stratifiedKfold(n_spilts=5)
+accuracy_on_stratiefiedKfold(x_to_y,test_derm_f,test_df.diagnosis_numeric,"x_to_y")
+
 
 # Concepts extraction from features (X=>C_HAT)
 # (X=>C_HAT) valid for both model, independent and sequential
@@ -53,6 +62,8 @@ for idx, t in enumerate(labels_cols):
     plt.close()
     # classification report to csv
     report_to_csv(C_HAT_test[idx], test_df[t], 'CBM/PredictedConcepts/' + t + '_result.csv')
+    # accuracy on stratifiedKfold(n_spilts=5) for each concept
+    accuracy_on_stratiefiedKfold(x_to_C_HAT[idx], test_derm_f, test_df[t], "x_to_" + t + "_HAT")
 
 # Predicted concepts for logistic regression training
 predicted_concepts_df_training = pd.DataFrame(C_HAT_train)
@@ -66,7 +77,6 @@ predicted_concepts_df.columns = labels_cols  # c_hat test df
 c_HAT_to_y = logReg(C=0.01, class_weight='balanced', train_inputs=predicted_concepts_df_training,
                     train_label=train_df.diagnosis_numeric)
 y_sequential_model = c_HAT_to_y.predict(predicted_concepts_df)
-
 # plot and save CM for DIAG
 plot_confusion(y_true=test_df.diagnosis_numeric, y_pred=y_sequential_model, labels=['NEV', 'MEL'], figsize=(6, 4))
 plt.title('DIAG_SeqMod' + ' - dermoscopic images');
@@ -74,17 +84,23 @@ plt.savefig('CBM/Diagnosis/' + 'DIAG' + '_CM' + '_SeqMod')
 plt.close()
 # classification report to csv
 report_to_csv(y_sequential_model, test_df.diagnosis_numeric, "CBM/Diagnosis/resultSeqMod.csv")
+# accuracy on stratifiedKfold(n_spilts=5)
+accuracy_on_stratiefiedKfold(c_HAT_to_y,predicted_concepts_df,test_df.diagnosis_numeric,"SeqMod")
+
 
 # Final prediction(NEV_or_MEL : DIAGNOSIS) based on real concepts (Independent Model: true_C => Y)
 c_to_y = logReg(C=0.01, class_weight='balanced', train_inputs=train_df[labels_cols],
                 train_label=train_df.diagnosis_numeric)
 y_independent_model = c_to_y.predict(predicted_concepts_df)
+accuracy_on_stratiefiedKfold(c_to_y,predicted_concepts_df,test_df.diagnosis_numeric,"IndMod")
 plot_confusion(y_true=test_df.diagnosis_numeric, y_pred=y_independent_model, labels=['NEV', 'MEL'], figsize=(6, 4))
 plt.title('DIAG_IndMod' + ' - dermoscopic images');
 plt.savefig('CBM/Diagnosis/' + 'DIAG' + '_CM' + '_IndMod')
 plt.close()
 # classification report to csv
 report_to_csv(y_independent_model, test_df.diagnosis_numeric, "CBM/Diagnosis/resultIndMod.csv")
+# accuracy on stratifiedKfold(n_spilts=5)
+accuracy_on_stratiefiedKfold(c_to_y,predicted_concepts_df,test_df.diagnosis_numeric,"IndMod")
 
 """7PointChecklist"""
 # 7pt based on true C
@@ -92,6 +108,7 @@ plot_confusion(y_true=test_df.diagnosis_numeric, y_pred=diagnosis(test_df, 3), l
 plt.title('DIAG_7ptChecklist' + ' - dermoscopic images');
 plt.savefig('CBM/Diagnosis/' + 'DIAG' + '_CM' + '_7pt_trueC')
 plt.close()
+
 # classification report to csv
 report_to_csv(diagnosis(test_df, 3), test_df.diagnosis_numeric, "CBM/Diagnosis/result7pt_trueC.csv")
 
